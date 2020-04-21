c68ee6cdf74ab5eb88d00c9c7f5ca607e7935dfb
gconnect <agatevureglory@gmail.com>
2020-04-21 13:31:08 +0100

    Completed the Quiz App challenge and added an alert to display user score

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/Question.dart';
+import 'package:quizzler/QuizBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,65 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+  int score = 0;
+  QuizBrain quizBrain = QuizBrain();
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswers = quizBrain.getCorrectAnswer();
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        if (score >= 50) {
+          alertWithButtons("Congratulations", score, AlertType.success);
+        } else {
+          alertWithButtons("You need to improve", score, AlertType.error);
+        }
+        quizBrain.resetQuiz();
+        scoreKeeper = [];
+        score = 0;
+      } else {
+        if (userPickedAnswer == correctAnswers) {
+          score = score + 10;
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+        } else {
+          score = score + 0;
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+        }
+        quizBrain.getNextQuestion();
+      }
+    });
+  }
+
+  void alertWithButtons(String message, int score, AlertType imagetype) {
+    Alert(
+      context: context,
+      type: imagetype,
+      title: message,
+      desc: "You scored $score",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "PLAY",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          color: Color.fromRGBO(0, 179, 134, 1.0),
+        ),
+        DialogButton(
+          child: Text(
+            "CANCEL",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(116, 116, 191, 1.0),
+            Color.fromRGBO(52, 138, 199, 1.0)
+          ]),
+        )
+      ],
+    ).show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +99,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +123,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                checkAnswer(true);
                 //The user picked true.
               },
             ),
@@ -79,19 +142,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                checkAnswer(false);
                 //The user picked false.
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
